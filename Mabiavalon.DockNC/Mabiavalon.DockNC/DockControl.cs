5425853d4360f21593dbafb1e68a305cc38ef3e6
Darnell Williams <williams.darnell56@yahoo.com>
2016-09-12 10:10:50 -0400

    Adds DockControl ready for customizations

---
+++ Mabiavalon.DockNC/Mabiavalon.DockNC/DockControl.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Controls.Primitives;
+using Avalonia.Media;
+
+namespace Mabiavalon.DockNC
+{
+	public class DockControl : DockPanel
+	{
+		public static readonly AttachedProperty<Dock> DockAtProperty =
+			AvaloniaProperty.RegisterAttached<DockControl, DockItem, Dock>("DockAt");
+
+		public static readonly StyledProperty<bool> LastChildFillProperty =
+			DockPanel.LastChildFillProperty.AddOwner<DockControl>();
+
+		public DockControl()
+		{
+			AffectsArrange(DockAtProperty);
+		}
+
+		public static Dock GetDockAt(DockItem dockItem)
+		{
+			return dockItem.GetValue(DockAtProperty);
+		}
+
+		public static void SetDockAt(DockItem dockItem, Dock value)
+		{
+			dockItem.SetValue(DockAtProperty, value);
+		}
+	}
+}
