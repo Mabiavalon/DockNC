2de62a5117e7609e199c2e3bb9ef2f99884ed01f
Darnell Williams <williams.darnell56@yahoo.com>
2016-09-13 13:31:03 -0700

    Update test code

--- Mabiavalon.DockNC/DockTestApplication/MainWindow.paml.cs
+++ Mabiavalon.DockNC/DockTestApplication/MainWindow.paml.cs
@@ -1,4 +1,5 @@
 ﻿using Avalonia.Controls;
+using Avalonia.Layout;
 using Avalonia.Markup.Xaml;
 using Mabiavalon.DockNC;
 
@@ -16,8 +17,17 @@ namespace DockTestApplication
                 var buttun = new Button();
                 buttun.Content = "TestButton In Left";
 
-                var docker = this.FindControl<DockControl>("Docker");
-                docker.Dock(buttun, Dock.Left);
+                buttun.Click += (o, eventArgs) =>
+                {
+                    var buttun2 = new Button();
+                    buttun2.Content = "TestButton In Left";
+
+                    var docker2 = this.FindControl<ContentControl>("Docker");
+                    (docker2.Content as Branch).SecondItem = buttun2;
+                };
+
+                var docker = this.FindControl<ContentControl>("Docker");
+                docker.Content = new Branch {FirstItem = buttun};
             };
         }
 
