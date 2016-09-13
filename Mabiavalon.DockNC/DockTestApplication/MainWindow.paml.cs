c4c573eb1eb061f629c8a87eccbdb389c890a950
Darnell Williams <williams.darnell56@yahoo.com>
2016-09-13 15:39:04 -0700

    Adds more to the test code

--- Mabiavalon.DockNC/DockTestApplication/MainWindow.paml.cs
+++ Mabiavalon.DockNC/DockTestApplication/MainWindow.paml.cs
@@ -1,7 +1,9 @@
 ﻿using Avalonia.Controls;
 using Avalonia.Layout;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
 using Mabiavalon.DockNC;
+using Serilog;
 
 namespace DockTestApplication
 {
@@ -15,12 +17,23 @@ namespace DockTestApplication
             button.Click += (sender, args) =>
             {
                 var buttun = new Button();
-                buttun.Content = "TestButton In Left";
+                buttun.Content = "TestButton Bottom";
 
                 buttun.Click += (o, eventArgs) =>
                 {
                     var buttun2 = new Button();
-                    buttun2.Content = "TestButton In Left";
+                    buttun2.Content = "TestButton In Top With Horizonal Branch";
+                    buttun2.Click += (sender1, routedEventArgs) =>
+                    {
+                        var docker3 = this.FindControl<ContentControl>("Docker");
+                        var branch = (docker3.Content as Branch);
+
+                        var horBranch = new Branch {Orientation = Orientation.Horizontal};
+                        horBranch.FirstItem = new Button {Content = "Left Button"};
+                        horBranch.SecondItem = new Button { Content = "Right Button"};
+
+                        branch.FirstItem = horBranch;
+                    };
 
                     var docker2 = this.FindControl<ContentControl>("Docker");
                     (docker2.Content as Branch).SecondItem = buttun2;
