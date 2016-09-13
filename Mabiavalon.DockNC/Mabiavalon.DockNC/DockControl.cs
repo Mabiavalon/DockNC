48a80176ab7a803c8ed10d9a902ffa9f7807fedd
Darnell Williams <williams.darnell56@yahoo.com>
2016-09-13 09:46:59 -0700

    Automatic Branch creation if content is null

--- Mabiavalon.DockNC/Mabiavalon.DockNC/DockControl.cs
+++ Mabiavalon.DockNC/Mabiavalon.DockNC/DockControl.cs
@@ -8,8 +8,31 @@ namespace Mabiavalon.DockNC
 {
 	public class DockControl : ContentControl
 	{
-		public DockControl()
-		{
-		}
+	    public void Dock(Control control, Dock dock)
+	    {
+	        if (Content == null)
+	        {
+	            switch (dock)
+	            {
+	                case Avalonia.Controls.Dock.Left:
+                        Content = new Branch { Orientation = Orientation.Horizontal, FirstItem = control };
+                        break;
+	                case Avalonia.Controls.Dock.Bottom:
+                        Content = new Branch { Orientation = Orientation.Vertical, SecondItem = control };
+                        break;
+	                case Avalonia.Controls.Dock.Right:
+                        Content = new Branch { Orientation = Orientation.Horizontal, SecondItem = control };
+	                    break;
+                    case Avalonia.Controls.Dock.Top:
+                        Content = new Branch { Orientation = Orientation.Vertical, FirstItem = control };
+	                    break;
+                    default:
+	                    throw new ArgumentOutOfRangeException(nameof(dock), dock, null);
+	            }
+	            return;             
+	        }
+
+
+        }
 	}
 }
