6102254fce79724e7faac77ae66c1481c312dbdb
Darnell Williams <williams.darnell56@yahoo.com>
2016-10-08 20:53:41 -0400

    Updates testapplication with splitting test

--- src/DockTestApplication/MainWindow.paml.cs
+++ src/DockTestApplication/MainWindow.paml.cs
@@ -1,55 +1,74 @@
-﻿using Avalonia.Controls;
-using Avalonia.Layout;
-using Avalonia.Logging.Serilog;
+﻿using System;
+using Avalonia.Controls;
+using Avalonia.LogicalTree;
 using Avalonia.Markup.Xaml;
 using Mabiavalon.DockNC;
-using Serilog;
 
 namespace DockTestApplication
 {
     public class MainWindow : Window
     {
-        public int ItemIteration { get; set; }
+        private readonly DockControl _docker;
 
         public MainWindow()
         {
-            this.InitializeComponent();
+            InitializeComponent();
             App.AttachDevTools(this);
 
             ItemIteration = 1;
 
-            var docker = this.FindControl<DockControl>("Docker");
-
+            _docker = this.FindControl<DockControl>("Docker");
 
             var topButton = this.FindControl<Button>("DockTop");
-            topButton.Click += (sender, args) =>
-            {
-                DockInternal(docker, DockTarget.Top);
-            };
-                          
+            topButton.Click += (sender, args) => { NestInternal(DockTarget.Top); };
+
             var bottomButton = this.FindControl<Button>("DockBottom");
-            bottomButton.Click += (sender, args) =>
-            {
-                DockInternal(docker, DockTarget.Bottom);
-            };
+            bottomButton.Click += (sender, args) => { NestInternal(DockTarget.Bottom); };
 
             var leftButtun = this.FindControl<Button>("DockLeft");
-            leftButtun.Click += (sender, args) =>
-            {
-                DockInternal(docker, DockTarget.Left);
-            };
+            leftButtun.Click += (sender, args) => { NestInternal(DockTarget.Left); };
 
             var rightButton = this.FindControl<Button>("DockRight");
-            rightButton.Click += (sender, args) =>
-            {
-                DockInternal(docker, DockTarget.Right);
-            };
+            rightButton.Click += (sender, args) => { NestInternal(DockTarget.Right); };
         }
 
-        private void DockInternal(DockControl docker, DockTarget dockTarget)
+        public int ItemIteration { get; set; }
+
+        private void NestInternal(DockTarget dockTarget)
         {
-            docker.Dock(new Button {Content = $"Item {ItemIteration}"}, dockTarget);
+            var button = CreateActionButton();
+
+            _docker.Dock(button, dockTarget);
             IterationItem();
+        }
+
+        private void SplitInternal(Branch branch, BranchItem branchItem)
+        {
+            var button = CreateActionButton();
+
+            _docker.Dock(button, RandomEnum<DockTarget>(), branch, branchItem);
+        }
+
+        private Button CreateActionButton()
+        {
+            var button = new Button {Content = $"Item {ItemIteration} (Splits in random position)"};
+            button.Click += Button_Click;
+
+            return button;
+        }
+
+        private void Button_Click(object sender, Avalonia.Interactivity.RoutedEventArgs e)
+        {
+            var button = (Button) sender;
+
+            if (button == null) return;
+
+            var parentBranch = button.GetLogicalParent<Branch>();
+
+            // Assumed for now, I see this not working as well as I think.
+            var branchItem = parentBranch.FirstItem == button ? BranchItem.First : BranchItem.Second;
+
+            SplitInternal(parentBranch, branchItem);
         }
 
         private void InitializeComponent()
@@ -60,6 +79,12 @@ namespace DockTestApplication
         private void IterationItem()
         {
             ItemIteration++;
+        }
+
+        public T RandomEnum<T>()
+        {
+            T[] values = (T[])Enum.GetValues(typeof(T));
+            return values[new Random().Next(0, values.Length)];
         }
     }
 }
