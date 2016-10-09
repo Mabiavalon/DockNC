4ee3c5f51f8885fd3e571085d6c14698da7feed6
Darnell Williams <williams.darnell56@yahoo.com>
2016-10-08 20:52:36 -0400

    Corrects logic and formatting in DockControl

--- src/Mabiavalon.DockNC/DockControl.cs
+++ src/Mabiavalon.DockNC/DockControl.cs
@@ -1,186 +1,187 @@
 ﻿using System;
-using Avalonia;
 using Avalonia.Controls;
-using Avalonia.Controls.Primitives;
-using Avalonia.Media;
 
 namespace Mabiavalon.DockNC
 {
-	public class DockControl : ContentControl
-	{
+    public class DockControl : ContentControl
+    {
         //TODO: Edge Case: Deal with empty Branches
-	    public void Dock(object obj, DockTarget dockTarget)
-	    {
-	        if (Content == null)
-	        {
+        public void Dock(object obj, DockTarget dockTarget)
+        {
+            if (Content == null)
+            {
                 var newBranch = new Branch();
 
                 SetOrientation(dockTarget, newBranch);
 
-	            if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
-	            {
-	                newBranch.FirstItem = obj;
-	            }
-	            else
-	            {
+                if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
+                {
+                    newBranch.FirstItem = obj;
+                }
+                else
+                {
                     newBranch.SecondItem = obj;
                 }
 
-	            Content = newBranch;
+                Content = newBranch;
                 return;
             }
 
-	        var currentBranch = Content as Branch;
+            var currentBranch = Content as Branch;
 
-	        if (currentBranch == null)
-	            throw new NotImplementedException("Support for wrapping pure content not supported yet.");
+            //TODO: THe logic is now in the second overload of dock, I just have to refactor to make it clean.
+            if (currentBranch == null)
+                throw new NotImplementedException("Support for wrapping pure content not supported yet.");
 
-	        if (currentBranch.BranchFilled)
-			{
-				var newBranch = new Branch();
-				Branch(obj, dockTarget, currentBranch, newBranch);
+            if (currentBranch.BranchFilled)
+            {
+                var newBranch = new Branch();
+                Branch(obj, dockTarget, currentBranch, newBranch);
 
-				Content = newBranch;
-			}
-			else
-			{
-				var emptyItem = GetEmptyLeaf(currentBranch);
+                Content = newBranch;
+            }
+            else
+            {
+                var emptyItem = GetEmptyLeaf(currentBranch);
 
-				var oldContent = emptyItem == BranchItem.First ? currentBranch.SecondItem : currentBranch.FirstItem;
+                var oldContent = emptyItem == BranchItem.First ? currentBranch.SecondItem : currentBranch.FirstItem;
 
-				// Possible solution to issues with visual parent?
-				currentBranch.FirstItem = null;
+                // Possible solution to issues with visual parent?
+                currentBranch.FirstItem = null;
                 currentBranch.FirstContentPresenter.UpdateChild();
-				currentBranch.SecondItem = null;
+                currentBranch.SecondItem = null;
                 currentBranch.SecondContentPresenter.UpdateChild();
 
-				Branch(obj, dockTarget, currentBranch, oldContent);
+                Branch(obj, dockTarget, currentBranch, oldContent);
 
-				currentBranch.FirstItemLength = new GridLength(0.49999, GridUnitType.Star);
-				currentBranch.SecondItemLength = new GridLength(0.50001, GridUnitType.Star);
-			}
-		}
+                currentBranch.FirstItemLength = new GridLength(0.49999, GridUnitType.Star);
+                currentBranch.SecondItemLength = new GridLength(0.50001, GridUnitType.Star);
+            }
+        }
 
-		public void Dock(object obj, DockTarget dockTarget, Branch branch, BranchItem branchItem)
-		{
-			object targetBranchContent = branchItem == BranchItem.First ? branch.FirstItem : branch.SecondItem;
+        public void Dock(object obj, DockTarget dockTarget, Branch branch, BranchItem branchItem)
+        {
+            var targetBranchContent = branchItem == BranchItem.First ? branch.FirstItem : branch.SecondItem;
 
-			if (!(targetBranchContent is Branch))
-			{
-			    if (branchItem == BranchItem.First)
-			    {
-			        branch.FirstItem = null;
-			        branch.FirstContentPresenter.UpdateChild();
-			    }
-			    else
-			    {
+            if (!(targetBranchContent is Branch))
+            {
+                if (branchItem == BranchItem.First)
+                {
+                    branch.FirstItem = null;
+                    branch.FirstContentPresenter.UpdateChild();
+                }
+                else
+                {
                     branch.SecondItem = null;
                     branch.SecondContentPresenter.UpdateChild();
                 }
 
-				var newBranch = new Branch();
+                var newBranch = new Branch();
 
-				SetOrientation(dockTarget, newBranch);
+                SetOrientation(dockTarget, newBranch);
 
-				if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
-				{
-					newBranch.FirstItem = obj;
-				}
-				else
-				{
-					newBranch.SecondItem = obj;
-				}
+                if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
+                {
+                    newBranch.FirstItem = obj;
+                    newBranch.SecondItem = targetBranchContent;
+                }
+                else
+                {
+                    newBranch.FirstItem = targetBranchContent;
+                    newBranch.SecondItem = obj;
+                }
 
-				if (branchItem == BranchItem.First)
-					branch.FirstItem = newBranch;
-				else
-					branch.SecondItem = newBranch;
+                if (branchItem == BranchItem.First)
+                    branch.FirstItem = newBranch;
+                else
+                    branch.SecondItem = newBranch;
 
-				return;
-			}
+                return;
+            }
 
-			var targetBranch = (Branch) targetBranchContent;
+            var targetBranch = (Branch) targetBranchContent;
 
-			if (targetBranch.BranchFilled)
-			{
-				if (branchItem == BranchItem.First)
-					branch.FirstItem = null;
-				else
-					branch.SecondItem = null;
+            if (targetBranch.BranchFilled)
+            {
+                if (branchItem == BranchItem.First)
+                    branch.FirstItem = null;
+                else
+                    branch.SecondItem = null;
 
-				var newBranch = new Branch();
+                var newBranch = new Branch();
 
-				Branch(obj, dockTarget, targetBranch, newBranch);
-			}
-			else
-			{
-				var emptyItem = GetEmptyLeaf(targetBranch);
+                Branch(obj, dockTarget, targetBranch, newBranch);
+            }
+            else
+            {
+                var emptyItem = GetEmptyLeaf(targetBranch);
 
-				var oldContent = emptyItem == BranchItem.First ? targetBranch.SecondItem : targetBranch.FirstItem;
+                var oldContent = emptyItem == BranchItem.First ? targetBranch.SecondItem : targetBranch.FirstItem;
 
-				// Possible solution to issues with visual parent?
-				targetBranch.FirstItem = null;
-				targetBranch.SecondItem = null;
+                // Possible solution to issues with visual parent?
+                targetBranch.FirstItem = null;
+                targetBranch.SecondItem = null;
 
-				Branch(obj, dockTarget, targetBranch, oldContent);
+                Branch(obj, dockTarget, targetBranch, oldContent);
 
-				targetBranch.FirstItemLength = new GridLength(0.49999, GridUnitType.Star);
-				targetBranch.SecondItemLength = new GridLength(0.50001, GridUnitType.Star);
-			}
-		}
+                targetBranch.FirstItemLength = new GridLength(0.49999, GridUnitType.Star);
+                targetBranch.SecondItemLength = new GridLength(0.50001, GridUnitType.Star);
+            }
+        }
 
-		private static void Branch(object obj, DockTarget dockTarget, Branch currentBranch, object oldContent)
-		{
-			SetOrientation(dockTarget, currentBranch);
+        private static void Branch(object obj, DockTarget dockTarget, Branch currentBranch, object oldContent)
+        {
+            SetOrientation(dockTarget, currentBranch);
 
-			if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
-			{
-				currentBranch.FirstItem = obj;
-				currentBranch.SecondItem = oldContent;
-			}
-			else
-			{
-				currentBranch.FirstItem = oldContent;
-				currentBranch.SecondItem = obj;
-			}
-		}
+            if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
+            {
+                currentBranch.FirstItem = obj;
+                currentBranch.SecondItem = oldContent;
+            }
+            else
+            {
+                currentBranch.FirstItem = oldContent;
+                currentBranch.SecondItem = obj;
+            }
+        }
 
-		private static void Branch(object obj, DockTarget dockTarget, Branch currentBranch, Branch newBranch)
-		{
-			SetOrientation(dockTarget, newBranch);
+        private static void Branch(object obj, DockTarget dockTarget, Branch currentBranch, Branch newBranch)
+        {
+            SetOrientation(dockTarget, newBranch);
 
-			if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
-			{
-				newBranch.FirstItem = obj;
-				newBranch.SecondItem = currentBranch;
-			}
-			else
-			{
-				newBranch.FirstItem = currentBranch;
-				newBranch.SecondItem = obj;
-			}
-		}
+            if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Left)
+            {
+                newBranch.FirstItem = obj;
+                newBranch.SecondItem = currentBranch;
+            }
+            else
+            {
+                newBranch.FirstItem = currentBranch;
+                newBranch.SecondItem = obj;
+            }
+        }
 
-		private static BranchItem GetEmptyLeaf(Branch branch)
-	    {
-	        if (branch.BranchFilled)
-	            throw new ArgumentException("Branch is filled", nameof(branch));
+        private static BranchItem GetEmptyLeaf(Branch branch)
+        {
+            if (branch.BranchFilled)
+                throw new ArgumentException("Branch is filled", nameof(branch));
 
-	        return branch.FirstItem == null ? BranchItem.First : BranchItem.Second;
-	    }
+            return branch.FirstItem == null ? BranchItem.First : BranchItem.Second;
+        }
 
-	    private static void SetOrientation(DockTarget dockTarget, Branch newBranch)
-	    {
-	        if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Bottom)
-	            newBranch.Orientation = Orientation.Vertical;
-	        else
-	            newBranch.Orientation = Orientation.Horizontal;
-	    }
+        private static void SetOrientation(DockTarget dockTarget, Branch newBranch)
+        {
+            if (dockTarget == DockTarget.Top || dockTarget == DockTarget.Bottom)
+                newBranch.Orientation = Orientation.Vertical;
+            else
+                newBranch.Orientation = Orientation.Horizontal;
+        }
 
-		private static Orientation GetOrientation(DockTarget dockTarget)
-		{
-			return dockTarget == DockTarget.Top || dockTarget == DockTarget.Bottom ? Orientation.Vertical : Orientation.Horizontal;
-		}
-
-	}
+        private static Orientation GetOrientation(DockTarget dockTarget)
+        {
+            return dockTarget == DockTarget.Top || dockTarget == DockTarget.Bottom
+                ? Orientation.Vertical
+                : Orientation.Horizontal;
+        }
+    }
 }
