c170df458ddb379525c4cd45e4791cc6cc01ed5d
Darnell Williams <williams.darnell56@yahoo.com>
2016-11-04 09:40:05 -0400

    [temp] enabled logging in DockTextApplication

--- src/DockTestApplication/App.paml.cs
+++ src/DockTestApplication/App.paml.cs
@@ -1,24 +1,42 @@
 ﻿using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Diagnostics;
+using Avalonia.Logging.Serilog;
 using Avalonia.Markup.Xaml;
+using Serilog;
 
 namespace DockTestApplication
 {
     internal class App : Application
     {
         public override void Initialize()
-        {
+        { 
             AvaloniaXamlLoader.Load(this);
             base.Initialize();
         }
 
-        private static void Main(string[] args) => AppBuilder.Configure<App>().UsePlatformDetect().Start<MainWindow>();
+        private static void Main(string[] args)
+        {
+            InitializeLogging();
+
+            AppBuilder.Configure<App>().UsePlatformDetect().Start<MainWindow>();
+        }
 
         public static void AttachDevTools(Window window)
         {
 #if DEBUG
             DevTools.Attach(window);
+#endif
+        }
+
+        // This will be made into a runtime configuration extension soon!
+        private static void InitializeLogging()
+        {
+#if DEBUG
+            SerilogLogger.Initialize(new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Trace(outputTemplate: "{Area}: {Message}")
+                .CreateLogger());
 #endif
         }
     }
