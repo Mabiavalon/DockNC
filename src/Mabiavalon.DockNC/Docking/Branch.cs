dbf959bedbe67b0782b50df152ae7c5c4331a4c7
Dan Walmsley <dan@walms.co.uk>
2016-11-11 09:26:10 +0000

    resolve conflicts.

--- src/Mabiavalon.DockNC/Docking/Branch.cs
+++ src/Mabiavalon.DockNC/Docking/Branch.cs
@@ -1,10 +1,11 @@
 ﻿namespace Mabiavalon.DockNC.Docking
 {
+    using System;
     using Avalonia;
     using Avalonia.Controls;
     using Avalonia.Controls.Presenters;
     using Avalonia.Controls.Primitives;
-    using System;
+    using System.Reactive.Disposables;
 
     public class Branch : TemplatedControl
     {
@@ -134,7 +135,10 @@
             UpdateLogicalChildren(null, FirstContentPresenter.Content);
             UpdateLogicalChildren(null, SecondContentPresenter.Content);
 
-            RegisterVisualChanges(FirstContentPresenter, ref _firstItemVisibilitDisposable);
+            InvalidateVisibilityChanges();
+            InvalidateMeasure();
+
+            RegisterVisualChanges(FirstContentPresenter, ref _firstItemVisibilityDisposable);
             RegisterVisualChanges(SecondContentPresenter, ref _secondItemVisibilityDisposable);
         }
 
